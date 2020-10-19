0b155b304fdda9b3afe9b1ef31558e1fc139896a
harrygillingham99 <harrygillingham99@gmail.com>
2020-10-19 18:31:09 +0100

    fix: move tile 3 down to second row

--- src/scripts/app/components/common/Dashboard.component.tsx
+++ src/scripts/app/components/common/Dashboard.component.tsx
@@ -15,9 +15,9 @@ export const Dashboard: React.FC<IDashboardProps> = () => {
         <Col className={"dash-item"}>
           <SecurityCamera />
         </Col>
-        <Col className={"dash-item"}>3</Col>
       </Row>
       <Row className={"dash-row"}>
+        <Col className={"dash-item"}>3</Col>
         <Col className={"dash-item"}>4</Col>
         <Col className={"dash-item dash-item--grow"}>5</Col>
       </Row>
