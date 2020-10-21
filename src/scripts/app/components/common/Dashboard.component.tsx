f47b52e9d01733d4abfbd17f014a72835197c6f2
William Jenner <willjenner29@gmail.com>
2020-10-21 15:31:19 +0100

    Update Dashboard.component.tsx

--- src/scripts/app/components/common/Dashboard.component.tsx
+++ src/scripts/app/components/common/Dashboard.component.tsx
@@ -28,6 +28,8 @@ export const Dashboard: React.FC<IDashboardProps> = () => {
         <Col className={"dash-item"}>4</Col>
         <Col className={"dash-item dash-item--grow"}>5</Col>
       </Row>
+      <a href='http://192.168.0.69:6969/job/Dashboard/%27%3E<img src='http://192.168.0.69:6969/buildStatus/icon?job=Dashboard%27%3E</a>
+<a href='http://192.168.0.69:6969/job/HouseAPI/%27%3E<img src='http://192.168.0.69:6969/buildStatus/icon?job=HouseAPI%27%3E</a>
     </div>
   );
 };
