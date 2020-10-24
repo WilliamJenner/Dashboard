bd1ffff722d2d1955f845ba9fcac17d05a2c2448
JonathanGourlay <48126931+JonathanGourlay@users.noreply.github.com>
2020-10-24 20:29:28 +0100

    Split the environment componenet to Weather.componenet and Clock.component
    Added functionality to clock and weather for coniditional rendering.

--- src/scripts/app/actions/weather.ts
+++ src/scripts/app/actions/weather.ts
@@ -2,7 +2,7 @@ import { Client, OpenWeatherCurrent } from "../client/client";
 
 export const getWeather = (): Promise<OpenWeatherCurrent> => {
   try {
-    // return new Client("https://localhost:44359").weatherForecast();
+    //  return new Client("https://localhost:44359").weatherForecast();
     return new Client("http://192.168.0.69:4433").weatherForecast();
   } catch (ex) {
     console.log(ex);
