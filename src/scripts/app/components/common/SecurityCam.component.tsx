5b2b959b4873783dc1551535085c300454e89dd2
will <willjenner29@gmail.com>
2022-01-15 17:34:33 +0000

    added loading to secruity cam

--- src/scripts/app/components/common/SecurityCam.component.tsx
+++ src/scripts/app/components/common/SecurityCam.component.tsx
@@ -1,10 +1,26 @@
+import { useInterval } from "@hooks/useInterval";
 import * as React from "react";
+import { Spinner } from "react-bootstrap";
+import { secondsToMilliseconds } from "../../../app/utils/number";
 import { AppState } from "../../state/appState";
 
 interface ISecurityCameraProps {}
 
 export const SecurityCamera: React.FC<ISecurityCameraProps> = () => {
   const { appState } = AppState.useContainer();
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  useInterval(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, secondsToMilliseconds(30));
+
+  if (loading === true) {
+    return <Spinner animation="border" />;
+  }
 
   return appState === undefined ? null : (
     <img className={"security-camera"} src={appState.securityCamUrl} />
