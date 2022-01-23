90f3d5c26e2516ec26e7398ad3783b1ef0db995c
harrygillingham99 <harrygillingham99@gmail.com>
2022-01-23 16:16:08 +0000

    fix(map): don't load every 5 minutes

--- src/scripts/app/components/common/BlueMap.component.tsx
+++ src/scripts/app/components/common/BlueMap.component.tsx
@@ -8,19 +8,6 @@ interface IBlueMapProps {}
 
 export const BlueMap : React.FC<IBlueMapProps> = () => {
   const { appState } = AppState.useContainer();
-  const [loading, setLoading] = React.useState<boolean>(false);
-
-  useInterval(() => {
-    setLoading(true);
-
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, secondsToMilliseconds(240));
-
-  if (loading === true) {
-    return <Spinner animation="border" />;
-  }
 
   return appState === undefined ? null : (
     <iframe className={"security-camera"} src={appState.blueMapUrl} />
